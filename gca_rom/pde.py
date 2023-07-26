c27b186082117f604683479bfe513210c99e9e5b
Federico Pichi <federico.pichi@epfl.ch>
2023-07-26 18:25:38 +0200

    Adding stokes tutorial and refactoring wrt multiparameter applications

--- gca_rom/pde.py
+++ gca_rom/pde.py
@@ -24,48 +24,75 @@ def problem(argument):
 
     match argument:
         case 1:
-            problem = "poisson"
+            problem_name = "poisson"
             variable = 'U'
             mu1 = np.linspace(0.01, 10., 10)
             mu2 = np.linspace(0.01, 10., 10)
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 2:
-            problem = "advection"
+            problem_name = "advection"
             variable = 'U'
             mu1 = np.linspace(0., 6., 10)
             mu2 = np.linspace(-1.0, 1.0, 10)
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 3:
-            problem = "graetz"
+            problem_name = "graetz"
             variable = 'U'
             mu1 = np.linspace(1., 3., 10)
             mu2 = np.linspace(0.01, 0.1, 20)
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 4:
-            problem = "navier_stokes"
+            problem_name = "navier_stokes"
             variable = 'VX'
             mu1 = np.linspace(0.5, 2., 21)[::2]
             mu2 = np.linspace(2., 0.5, 151)[::5]
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 5:
-            problem = "navier_stokes"
+            problem_name = "navier_stokes"
             variable = 'VY'
             mu1 = np.linspace(0.5, 2., 21)[::2]
             mu2 = np.linspace(2., 0.5, 151)[::5]
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 6:
-            problem = "navier_stokes"
+            problem_name = "navier_stokes"
             variable = 'P'
             mu1 = np.linspace(0.5, 2., 21)[::2]
             mu2 = np.linspace(2., 0.5, 151)[::5]
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 7:
-            problem = "diffusion"
+            problem_name = "diffusion"
             variable = 'U'
             mu1 = np.linspace(0.2, 4., 20)
             mu2 = np.linspace(0., 1., 20)
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 8:
-            problem = "poiseuille"
+            problem_name = "poiseuille"
             variable = 'U'
             mu1 = np.linspace(0.5, 10., 20)
             mu2 = np.linspace(0., 1., 50)
+            mu_space = [mu1, mu2]
+            n_param = 2
         case 9:
-            problem = "elasticity"
+            problem_name = "elasticity"
             variable = 'U'
             mu1 = np.linspace(2., 20., 11)
             mu2 = np.linspace(2., 200., 11)
-    return problem, variable, mu1, mu2
+            mu_space = [mu1, mu2]
+            n_param = 2
+        case 10:
+            problem_name = "stokes_u"
+            variable = 'U'
+            mu_range = [(0.5, 1.5), (0.5, 1.5), (0.5, 1.5), (0.5, 1.5), (0.5, 1.5), (-np.pi/6, np.pi/6), (-10, 10)]
+            mu_space = []
+            n_pts = [2]*(len(mu_range)-1)+[11]
+            for i in range(len(mu_range)):
+                mu_space.append(np.linspace(mu_range[i][0], mu_range[i][1], n_pts[i]))
+            n_param = 7
+    return problem_name, variable, mu_space, n_param
