0c0e31cdbfc642afd9357a8e98a1342fbb23ebe3
Federico Pichi <federico.pichi@epfl.ch>
2023-08-03 17:41:34 +0200

    Add tqdm for training and refactoring code

--- gca_rom/training.py
+++ gca_rom/training.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm 
 
-
-def train(model, optimizer, device, scheduler, params, train_loader, train_trajectories, HyperParams, history):
+def train(model, optimizer, device, scheduler, params, train_loader, test_loader, train_trajectories, test_trajectories, HyperParams):
     """Trains the autoencoder model.
     
     This function trains the autoencoder model using mean squared error (MSE) loss and a map loss, where the map loss
@@ -25,77 +26,78 @@ def train(model, optimizer, device, scheduler, params, train_loader, train_traje
     Returns:
         float: The average loss over all training examples.
     """
+    train_history = dict(train=[], l1=[], l2=[])
+    test_history = dict(test=[], l1=[], l2=[])
+    min_test_loss = np.Inf
 
     model.train()
-    total_loss_train = total_examples = sum_loss = 0
-    total_loss_train_1 = total_loss_train_2 = 0
-    sum_loss_1 = sum_loss_2 = 0
-
-    for data in train_loader:
-        optimizer.zero_grad()
-        data = data.to(device)  
-        out, z, z_estimation = model(data, params[train_trajectories, :])
-        loss_train_mse = F.mse_loss(out, data.x, reduction='mean')
-        loss_train_map = F.mse_loss(z_estimation, z, reduction='mean')
-        loss_train = loss_train_mse + HyperParams.lambda_map * loss_train_map
-        loss_train.backward()
-        optimizer.step()
-        sum_loss += loss_train.item()
-        sum_loss_1 += loss_train_mse.item()
-        sum_loss_2 += loss_train_map.item()
-        total_examples += 1
-
-    scheduler.step()
-    total_loss_train = sum_loss / total_examples
-    total_loss_train_1 = sum_loss_1 / total_examples
-    total_loss_train_2 = sum_loss_2 / total_examples
-    history['train'].append(total_loss_train)
-    history['l1'].append(total_loss_train_1)
-    history['l2'].append(total_loss_train_2)
-    return total_loss_train
-
-
-def val(model, device, params, test_loader, test_trajectories, HyperParams, history_test):
-    """
-    Evaluate the performance of a model on a test set.
-
-    This function calculates the mean of the total loss, the mean of loss_test_mse and the mean of loss_test_map for all test examples. The losses are computed as the mean squared error (MSE) between the model's predictions and the true target variables, and between the estimated latent code and the true latent code. The lambda_map weight balances the contribution of each loss term to the total loss. The function adds the computed loss values to the history_test dictionary.
-
-    Parameters:
-        model (torch.nn.Module): The model to be evaluated.
-        device (torch.device): The device to use for computations (e.g. 'cpu' or 'cuda').
-        params (torch.Tensor): Tensor containing the parameters of the model.
-        test_loader (torch.utils.data.DataLoader): The test data to use for evaluation.
-        test_trajectories (int): The index of the test trajectory.
-        HyperParams (object): Object containing hyperparameters for the model.
-        history_test (dict): Dictionary to store the evaluation results.
-
-    Returns:
-        float: The mean of the total loss computed over all test examples.
-    """
-
-    with torch.no_grad():
-        model.eval()
-
-        total_loss_test = total_examples = sum_loss = 0
-        total_loss_test_1 = total_loss_test_2 = 0
+    loop = tqdm(range(HyperParams.max_epochs))
+    for epoch in loop:
+        train_rmse = total_examples = sum_loss = 0
+        train_rmse_1 = train_rmse_2 = 0
         sum_loss_1 = sum_loss_2 = 0
-        for data in test_loader:
-            data = data.to(device)
-            out, z, z_estimation = model(data, params[test_trajectories, :])
-            loss_test_mse = F.mse_loss(out, data.x, reduction='mean')
-            loss_test_map = F.mse_loss(z_estimation, z, reduction='mean')
-            loss_test = loss_test_mse +  HyperParams.lambda_map * loss_test_map
-            sum_loss += loss_test.item()
-            sum_loss_1 += loss_test_mse.item()
-            sum_loss_2 += loss_test_map.item()
+
+        for data in train_loader:
+            optimizer.zero_grad()
+            data = data.to(device)  
+            out, z, z_estimation = model(data, params[train_trajectories, :])
+            loss_train_mse = F.mse_loss(out, data.x, reduction='mean')
+            loss_train_map = F.mse_loss(z_estimation, z, reduction='mean')
+            loss_train = loss_train_mse + HyperParams.lambda_map * loss_train_map
+            loss_train.backward()
+            optimizer.step()
+            sum_loss += loss_train.item()
+            sum_loss_1 += loss_train_mse.item()
+            sum_loss_2 += loss_train_map.item()
             total_examples += 1
 
-        total_loss_test = sum_loss / total_examples
-        total_loss_test_1 = sum_loss_1 / total_examples
-        total_loss_test_2 = sum_loss_2 / total_examples
-        history_test['test'].append(total_loss_test)
-        history_test['l1'].append(total_loss_test_1)
-        history_test['l2'].append(total_loss_test_2)
-        return total_loss_test
+        scheduler.step()
+        train_rmse = sum_loss / total_examples
+        train_rmse_1 = sum_loss_1 / total_examples
+        train_rmse_2 = sum_loss_2 / total_examples
+        train_history['train'].append(train_rmse)
+        train_history['l1'].append(train_rmse_1)
+        train_history['l2'].append(train_rmse_2)
 
+        if HyperParams.cross_validation:
+            with torch.no_grad():
+                model.eval()
+                test_rmse = total_examples = sum_loss = 0
+                test_rmse_1 = test_rmse_2 = 0
+                sum_loss_1 = sum_loss_2 = 0
+                for data in test_loader:
+                    data = data.to(device)
+                    out, z, z_estimation = model(data, params[test_trajectories, :])
+                    loss_test_mse = F.mse_loss(out, data.x, reduction='mean')
+                    loss_test_map = F.mse_loss(z_estimation, z, reduction='mean')
+                    loss_test = loss_test_mse +  HyperParams.lambda_map * loss_test_map
+                    sum_loss += loss_test.item()
+                    sum_loss_1 += loss_test_mse.item()
+                    sum_loss_2 += loss_test_map.item()
+                    total_examples += 1
+
+                test_rmse = sum_loss / total_examples
+                test_rmse_1 = sum_loss_1 / total_examples
+                test_rmse_2 = sum_loss_2 / total_examples
+                test_history['test'].append(test_rmse)
+                test_history['l1'].append(test_rmse_1)
+                test_history['l2'].append(test_rmse_2)
+            # print("Epoch[{}/{}, train_mse loss:{}, test_mse loss:{}".format(epoch + 1, HyperParams.max_epochs, train_history['train'][-1], test_history['test'][-1]))
+            loop.set_postfix({"Loss(training)": train_history['train'][-1]}, {"Loss(validation)": test_history['test'][-1]})
+        else:
+            test_rmse = train_rmse
+            # print("Epoch[{}/{}, train_mse loss:{}".format(epoch + 1, HyperParams.max_epochs, train_history['train'][-1]))
+            loop.set_postfix({"Loss(training)": train_history['train'][-1]})
+
+        if test_rmse < min_test_loss:
+            min_test_loss = test_rmse
+            best_epoch = epoch
+            torch.save(model.state_dict(), HyperParams.net_dir+HyperParams.net_name+HyperParams.net_run+'.pt')
+        if HyperParams.tolerance >= train_rmse:
+            print('Early stopping!')
+            break
+        np.save(HyperParams.net_dir+'history'+HyperParams.net_run+'.npy', train_history)
+        np.save(HyperParams.net_dir+'history_test'+HyperParams.net_run+'.npy', test_history)
+    
+    print("\nLoading best network for epoch: ", best_epoch)
+    model.load_state_dict(torch.load(HyperParams.net_dir+HyperParams.net_name+HyperParams.net_run+'.pt', map_location=torch.device('cpu')))
