7d2ee836ff4b592ea1aab2e84a48b140e075cd93
Federico Pichi <federico.pichi@epfl.ch>
2023-12-11 17:28:42 +0100

    Change plotting functions and add 2D scatter error plot
    Co-authored-by: Francesco Sala <francesco.sala@epfl.ch>

--- gca_rom/plotting.py
+++ gca_rom/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from gca_rom import scaling
 from collections import defaultdict
 import matplotlib.gridspec as gridspec
-import matplotlib.cm as cm
+from matplotlib import colormaps
 from matplotlib import ticker
 from matplotlib.ticker import MaxNLocator
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -15,6 +15,7 @@ params = {'legend.fontsize': 'x-large',
          'xtick.labelsize':'x-large',
          'ytick.labelsize':'x-large'}
 plt.rcParams.update(params)
+
 
 def plot_loss(HyperParams):
     """
@@ -34,7 +35,7 @@ def plot_loss(HyperParams):
     plt.ylabel('Loss')
     plt.xlabel('Epochs')
     plt.title('Loss over training epochs')
-    plt.legend(['loss_mse', 'loss_map', 'loss_test_mse', 'loss_test_map'])
+    plt.legend(['Autoencoder (train)', 'Map (train)', 'Autoencoder (test)', 'Map (test)'])
     plt.savefig(HyperParams.net_dir+'history_losses'+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
 
 
@@ -57,7 +58,6 @@ def plot_latent(HyperParams, latents, latents_estimation):
     plt.xlabel('Snaphots')
     plt.legend(['Autoencoder', 'Map'])
     plt.savefig(HyperParams.net_dir+'latents'+HyperParams.net_run+'.png', bbox_inches='tight', dpi=500)
-    
     green_diamond = dict(markerfacecolor='g', marker='D')
     _, ax = plt.subplots()
     ax.boxplot(latents_estimation.detach().numpy(), flierprops=green_diamond)
@@ -103,21 +103,64 @@ def plot_error(res, VAR_all, scaler_all, HyperParams, mu_space, params, train_tr
     output = np.reshape(error, (len(mu1_range), len(mu2_range)))
     fig = plt.figure('Relative Error '+vars)
     ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X1, X2, output, cmap=cm.coolwarm, color='blue')
-    ax.contour(X1, X2, output, zdir='z', offset=output.min(), cmap=cm.coolwarm)
+    ax.plot_surface(X1, X2, output, cmap=colormaps['coolwarm'], color='blue')
+    ax.contour(X1, X2, output, zdir='z', offset=output.min(), cmap=colormaps['coolwarm'])
     ax.set(xlim=tuple([mu1_range[0], mu1_range[-1]]),
            ylim=tuple([mu2_range[0], mu2_range[-1]]),
            xlabel=f'$\mu_{str((p1%n_params)+1)}$',
            ylabel=f'$\mu_{str((p2%n_params)+1)}$',
            zlabel='$\\epsilon_{GCA}(\\mathbf{\mu})$')
     ax.plot(tr_pt_1, tr_pt_2, output.min()*np.ones(len(tr_pt_1)), '*r')
-    plt.ticklabel_format(axis='z', style='sci', scilimits=(0, 0))
-    ax.set_title('Relative Error '+vars)
     ax.zaxis.offsetText.set_visible(False)
     exponent_axis = np.floor(np.log10(max(ax.get_zticks()))).astype(int)
-    ax.text2D(0.9, 0.82, "1e"+str(exponent_axis), transform=ax.transAxes, fontsize="x-large")
+    ax.ticklabel_format(axis='z', style='sci', scilimits=(0, 0))
+    ax.text2D(0.9, 0.82, "$\\times 10^{"+str(exponent_axis)+"}$", transform=ax.transAxes, fontsize="x-large")
+    plt.subplots_adjust(right=0.8)
     plt.tight_layout()
     plt.savefig(HyperParams.net_dir+'relative_error_'+vars+HyperParams.net_run+'.png', transparent=True, dpi=500)
+
+
+def plot_error_2d(res, VAR_all, scaler_all, HyperParams, mu_space, params, train_trajectories, vars, p1=0, p2=-1):
+    """
+    This function plots the relative error between the predicted and actual results in 2D
+
+    Parameters:
+    res (ndarray): The predicted results
+    VAR_all (ndarray): The actual results
+    scaler_all (object): The scaler object used for scaling the results
+    HyperParams (object): The HyperParams object holding the necessary hyperparameters
+    mu1_range (ndarray): Range of the first input variable
+    mu2_range (ndarray): Range of the second input variable
+    params (ndarray): The input variables
+    train_trajectories (ndarray): The indices of the training data
+    vars (str): The name of the variable being plotted
+    """
+
+    u_hf = scaling.inverse_scaling(VAR_all, scaler_all, HyperParams.scaling_type)
+    u_app = scaling.inverse_scaling(res, scaler_all, HyperParams.scaling_type)
+    error = np.linalg.norm(u_app - u_hf, axis=0) / np.linalg.norm(u_hf, axis=0)
+    mu1_range = mu_space[p1]
+    mu2_range = mu_space[p2]
+    n_params = params.shape[1]
+    tr_pt_1 = params[train_trajectories, p1]
+    tr_pt_2 = params[train_trajectories, p2]
+    X1, X2 = np.meshgrid(mu1_range, mu2_range, indexing='ij')
+    output = np.reshape(error, (len(mu1_range), len(mu2_range)))
+    fig = plt.figure('Relative Error 2D'+vars)
+    ax = fig.add_subplot()
+    fmt = ticker.ScalarFormatter(useMathText=True)
+    fmt.set_powerlimits((0, 0))
+    colors = output.flatten()
+    sc = plt.scatter(X1.flatten(), X2.flatten(), s=(2e1*colors/output.max())**2, c=colors, cmap=colormaps['coolwarm'])
+    plt.colorbar(sc, format=fmt)
+    ax.set(xlim=tuple([mu1_range[0], mu1_range[-1]]),
+           ylim=tuple([mu2_range[0], mu2_range[-1]]),
+           xlabel=f'$\mu_{str((p1%n_params)+1)}$',
+           ylabel=f'$\mu_{str((p2%n_params)+1)}$')
+    ax.plot(tr_pt_1, tr_pt_2,'*r')
+    plt.tight_layout()
+    plt.savefig(HyperParams.net_dir+'relative_error_2d_'+vars+HyperParams.net_run+'.png', transparent=True, dpi=500)
+    plt.show()
 
 
 def plot_fields(SNAP, results, scaler_all, HyperParams, dataset, xyz, params):
@@ -141,30 +184,30 @@ def plot_fields(SNAP, results, scaler_all, HyperParams, dataset, xyz, params):
     z_net = Z_net[:, SNAP]
     xx = xyz[0]
     yy = xyz[1]
+    fmt = ticker.ScalarFormatter(useMathText=True)
+    fmt.set_powerlimits((0, 0))
     if dataset.dim == 2:
         triang = np.asarray(dataset.T - 1)
-        cmap = cm.get_cmap(name='jet', lut=None)
         gs1 = gridspec.GridSpec(1, 1)
         ax = plt.subplot(gs1[0, 0])
-        cs = ax.tricontourf(xx[:, SNAP], yy[:, SNAP], triang, z_net, 100, cmap=cmap)
+        cs = ax.tricontourf(xx[:, SNAP], yy[:, SNAP], triang, z_net, 100, cmap=colormaps['jet'])
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
-        cbar = plt.colorbar(cs, cax=cax)
+        cbar = plt.colorbar(cs, cax=cax, format=fmt)
     elif dataset.dim == 3:
         zz = xyz[2]
         ax = fig.add_subplot(projection='3d')
         cax = inset_axes(ax, width="5%", height="60%", loc="center left", 
                          bbox_to_anchor=(1.15, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0)
-        cmap = cm.get_cmap(name='jet', lut=None) 
-        p = ax.scatter(xx[:, SNAP], yy[:, SNAP], zz[:, SNAP], c=z_net, cmap=cmap, linewidth=0.5)
-        cbar = fig.colorbar(p, ax=ax, cax=cax)
+        p = ax.scatter(xx[:, SNAP], yy[:, SNAP], zz[:, SNAP], c=z_net, cmap=colormaps['jet'], linewidth=0.5)
+        cbar = fig.colorbar(p, ax=ax, cax=cax, format=fmt)
         ax.set_xlabel('$x$')
         ax.set_ylabel('$y$')
         ax.set_zlabel('$z$')
         ax.locator_params(axis='both', nbins=5)
     tick_locator = MaxNLocator(nbins=5)
     cbar.locator = tick_locator
-    cbar.formatter.set_powerlimits((0, 0))
+    cbar.ax.yaxis.set_offset_position('left')  
     cbar.update_ticks()
     plt.tight_layout()
     ax.set_aspect('equal', 'box')
@@ -197,30 +240,30 @@ def plot_error_fields(SNAP, results, VAR_all, scaler_all, HyperParams, dataset, 
     error = abs(z - z_net)/np.linalg.norm(z, 2)
     xx = xyz[0]
     yy = xyz[1]
+    fmt = ticker.ScalarFormatter(useMathText=True)
+    fmt.set_powerlimits((0, 0))
     if dataset.dim == 2:
         triang = np.asarray(dataset.T - 1)
-        cmap = cm.get_cmap(name='jet', lut=None) 
         gs1 = gridspec.GridSpec(1, 1)
-        ax = plt.subplot(gs1[0, 0])   
-        cs = ax.tricontourf(xx[:, SNAP], yy[:, SNAP], triang, error, 100, cmap=cmap)
+        ax = plt.subplot(gs1[0, 0])
+        cs = ax.tricontourf(xx[:, SNAP], yy[:, SNAP], triang, error, 100, cmap=colormaps['coolwarm'])
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.1)
-        cbar = plt.colorbar(cs, cax=cax)
+        cbar = plt.colorbar(cs, cax=cax, format=fmt)
     elif dataset.dim == 3:
         zz = xyz[2]
         ax = fig.add_subplot(projection='3d')
         cax = inset_axes(ax, width="5%", height="60%", loc="center left", 
                          bbox_to_anchor=(1.15, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0)
-        cmap = cm.get_cmap(name='jet', lut=None) 
-        p = ax.scatter(xx[:, SNAP], yy[:, SNAP], zz[:, SNAP], c=error, cmap=cmap, linewidth=0.5)
-        cbar = fig.colorbar(p, ax=ax, cax=cax)
+        p = ax.scatter(xx[:, SNAP], yy[:, SNAP], zz[:, SNAP], c=error, cmap=colormaps['coolwarm'], linewidth=0.5)
+        cbar = fig.colorbar(p, ax=ax, cax=cax, format=fmt)
         ax.set_xlabel('$x$')
         ax.set_ylabel('$y$')
         ax.set_zlabel('$z$')
         ax.locator_params(axis='both', nbins=5)
     tick_locator = MaxNLocator(nbins=5)
     cbar.locator = tick_locator
-    cbar.formatter.set_powerlimits((0, 0))
+    cbar.ax.yaxis.set_offset_position('left')  
     cbar.update_ticks()
     plt.tight_layout()
     ax.set_aspect('equal', 'box')
